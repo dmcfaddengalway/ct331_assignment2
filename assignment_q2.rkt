caf14f53a26c41e19d5a9cfb10c32a69f62e796c
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-10-27 13:20:48 +0100

    ins_beg and ins_end working removed comments added mine

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -5,18 +5,15 @@
 
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
 (provide ins_beg)
+(provide ins_end)
 
+;; Append an item to the beginning of the list
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (append (list el) lst)
+)
+
+;; Append an item to the end of the list...opposite order of ins_beg
+(define (ins_end el lst)
+  (append lst (list el))
+)
